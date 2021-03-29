59f8c299271ae4c3c0bdbb229630ba87b9b71223
Kevin Quiercelin <quiercelinkevin@gmail.com>
2021-03-29 17:01:28 +0200

    Commit Message d'erreur

--- TPLectures/Models/AjouterAdminViewModel.cs
+++ TPLectures/Models/AjouterAdminViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using TPLectureDB.EF.Models;
@@ -12,8 +13,13 @@ namespace TPLectures.Web.Models
         public String Titre { get; set; }
         public int Identifiant { get; set; }
         public int NombrePages { get; set; }
+        [Required]
         public string Edition { get; set; }
+       
+        
+        [Required(ErrorMessage = "L'ISBN est obligatoire !!")]
         public string Isbn { get; set; }
+       
         public short DateEdition { get; set; }
         public DateTime? DateLecture { get; set; }
         public int? Note { get; set; }
